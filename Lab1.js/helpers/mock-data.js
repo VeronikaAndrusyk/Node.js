cc7af9dadc0174a8c7dfb93dd609deeb5f6446d2
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2024-02-27 09:00:02 +0200

    Update mock-data.js(id)

--- Lab1.js/helpers/mock-data.js
+++ Lab1.js/helpers/mock-data.js
@@ -1,6 +1,6 @@
 const interns = [
   {
-    id: "3689e68ad161e733c560a40d7af78114",
+    id: "1qw2",
     lastName: "Smith",
     birthdate: "2022-10-11",
     gender: "A",
@@ -9,7 +9,7 @@ const interns = [
     city: "London",
   },
   {
-    id: "1e80852dca982178b3d7eabffa54382e",
+    id: "0qw1",
     lastName: "Johnson",
     birthdate: "1992-11-11",
     gender: "A",
@@ -17,7 +17,7 @@ const interns = [
     workPlace: "Microsost",
     city: "Italy",
   {
-    id: "c0a7c2a8d715a009397677b3e1a40b18",
+    id: "c0a",
     lastName: "Williams",
     birthdate: "2005-09-01",
     gender: "B",
