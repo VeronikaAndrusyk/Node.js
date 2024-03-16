6ad1931384a93a968bc9a41fd436cdeee575c997
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2024-03-16 17:54:09 +0200

    Update interns.service.js

--- labka2/services/interns.service.js
+++ labka2/services/interns.service.js
@@ -2,58 +2,35 @@ const internModel = require("../models/intern.model");
 
 
 async function create(intern) {
-  try {
-    const newIntern = { id: _generateId(), ...intern };
-    await internModel.create(newIntern); // вик метод create моделі Mongoose для створення нового інтерна
-    return newIntern;
-  } catch (err) {
-    throw err; //  помилка далі для обробки 
-  }
+ return internModel.create(intern)
 }
 
 async function find({
-  searchString = "",
-  page = 1,
-  perPage = Number.MAX_SAFE_INTEGER,
-}) {
-  try {
-    const searchResult = await internModel.find(); // вик. метод find моделі Mongoose для пошуку 
-    const count = await internModel.countDocuments(); //  заг кільк
-    const items = searchResult.slice((page - 1) * perPage, page * perPage); // виб необхідну сторінку
-    return { items, count };
-  } catch (err) {
-    throw err;
+   const filter = {
+    lastName: { $regex: `^${searchString}`, $options: 'i' },
+  };
+  return {
+    items: await internModel.find(filter).skip((page - 1) * perPage).limit(Number(perPage)),
+    count: await internModel.countDocuments(filter),
   }
 }
 
 async function findById(id) {
-  try {
-    return await internModel.findById(id); 
-  } catch (err) {
-    throw err;
-  }
+ return internModel.findById(id);
 }
 
-async function update(internId, internData) {
-  try {
-    await internModel.findByIdAndUpdate(internId, internData); 
-  } catch (err) {
-    throw err;
-  }
-}
+async function findByIdAndUpdate(id, update) {
+  return internModel.findByIdAndUpdate(id, update, { upsert: false, new: true });
+};
 
-async function remove(id) {
-  try {
-    await internModel.findByIdAndRemove(id); 
-  } catch (err) {
-    throw err;
-  }
-}
+async function findByIdAndDelete(id) {
+  return internModel.findByIdAndDelete(id);
+};
 
 module.exports = {
   create,
   find,
   findById,
-  update,
-  remove,
+  findByIdAndUpdate,
+  findByIdAndDelete,
 };
