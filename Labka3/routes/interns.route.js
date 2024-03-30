1bc29073843fc1edc892d6ee8d06f3f9cfeec4a1
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2024-03-30 16:47:17 +0200

    Update interns.route.js

--- Labka3/routes/interns.route.js
+++ Labka3/routes/interns.route.js
@@ -7,16 +7,15 @@ const middleware = require("../middlewares/interns.middleware");
 router
   .route("/") // маршрут для HTTP-запитів на кореневу сторінку.
   .get(controller.getInterns)
-  .post(controller.createIntern);
+  .post(middleware.validateGroup,controller.createIntern);
 
-router.route("/lastName/:internLastName") //переставлення цього маршруту перед маршрутами з :internId
-  .get(middleware.internByIdValidation, controller.getInternByLastName);
+
 
 router
   .route("/:internId")
-  .get(middleware.internByIdValidation, controller.getIntern)
-  .put(middleware.internByIdValidation, controller.updateIntern)
-  .patch(middleware.internByIdValidation, controller.updateIntern)
-  .delete(middleware.internByIdValidation, controller.deleteIntern);
+  .get(middleware.validateGroup, controller.getIntern)
+  .put(middleware.validateGroup, controller.updateIntern)
+  .patch(middleware.validateGroup, controller.updateIntern)
+  .delete(middleware.validateGroup, controller.deleteIntern);
 
 module.exports = router;
