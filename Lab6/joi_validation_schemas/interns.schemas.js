7110d13282f5b01d5c37abac6fdffde071ea864b
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2024-04-26 08:31:38 +0300

    Add files via upload

---
+++ Lab6/joi_validation_schemas/interns.schemas.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+
+const InternSchema = Joi.object({
+   
+    lastName: Joi.string()
+        .min(2)
+        .max(30),
+        
+
+    birthdate: Joi.date(),
+        
+
+    group: Joi.string()
+        .valid('A', 'B', 'C'),
+
+    faculty: Joi.string()
+        .min(2)
+        .max(30),
+
+    average: Joi.number()
+        .min(0)
+        .max(100), 
+
+    workplace: Joi.string()
+        .min(2)
+        .max(100),
+
+    city: Joi.string()
+        .min(2)
+        .max(30),
+});
+
+module.exports = {
+    InternSchema,
+    
+};
